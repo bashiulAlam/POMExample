b09a44b7d7a3cec25be96d5874903f33c349fdb4
bashiulAlam <sabab.ndc@gmail.com>
2020-09-18 21:07:37 +0600

    added google/facebook page test cases

--- src/test/java/pom/test/TestBase.java
+++ src/test/java/pom/test/TestBase.java
@@ -1,18 +1,17 @@
 package pom.test;
 
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sabab on 9/16/2020.
  */
 public class TestBase {
-    public static String baseUrl = "https://lp-portal.konapay.net:13000/portal/pp";
+    //public static String baseUrl = "https://lp-portal.konapay.net:13000/portal/pp";
+    //public static String baseUrl = "https://www.google.com/";
+    public static String baseUrl = "https://www.facebook.com/";
     public static WebDriver driver = null;
 
     @BeforeClass
